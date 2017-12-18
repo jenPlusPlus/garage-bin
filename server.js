05371ffbb85b31e28ed1e680e54c8a10296ab794
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-12-18 11:05:07 -0700

    Add GET all endpoint

--- server.js
+++ server.js
@@ -18,6 +18,23 @@ const database = require('knex')(configuration);
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/api/v1/items', (request, response) => {
+  database('garage_items').select()
+    .then(garageItems => response.status(200).json({ garageItems }))
+    .catch(error => response.status(500).json({ error }));
+});
+
+// app.get('/api/v1/teams/:id', (request, response) => {
+//   database('teams').where('id', request.params.id).select()
+//     .then((team) => {
+//       if (team.length) {
+//         return response.status(200).json({ team: team[0] });
+//       }
+//       return response.status(404).json({ error: `Could not find any team associated with id ${request.params.id}` });
+//     })
+//     .catch(error => response.status(500).json({ error }));
+// });
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}`);
 });
